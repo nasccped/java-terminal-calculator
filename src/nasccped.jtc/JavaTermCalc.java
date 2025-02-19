b44d860da8a13f0112bcdd4b095eb9d75b0c5360
NASCC <pedro.main.tec@gmail.com>
2025-02-19 17:28:43 -0300

    [ADDED/UPDATED] Using cool utils

--- src/nasccped.jtc/JavaTermCalc.java
+++ src/nasccped.jtc/JavaTermCalc.java
@@ -1,8 +1,14 @@
 package nasccped.jtc;
 
+import static nasccped.jtc.Visuals.clearTerminal;
+
 public class JavaTermCalc {
 
     public static void main(String[] args) {
-        System.out.println("Hello, calculator!");
+        clearTerminal();
+        System.out.println();
+        System.out.println("  Hello, calculator,");
+        System.out.println("  From a \u001b[1;34mcool terminal\u001b[0m!");
+        System.out.println();
     }
 }
---
+++ src/nasccped.jtc/Visuals.java
@@ -0,0 +1,7 @@
+package nasccped.jtc;
+
+public class Visuals {
+    protected static void clearTerminal() {
+        System.out.print("\u001b[2J\u001b[H");
+    }
+}
