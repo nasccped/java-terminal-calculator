88548ce9e009479816ddc717a71bd261dc9418d9
NASCC <pedro.main.tec@gmail.com>
2025-02-19 17:57:04 -0300

    [ADDED] System's printing alias

---
+++ src/nasccped.jtc/utils/IO.java
@@ -0,0 +1,15 @@
+package nasccped.jtc.utils;
+
+public class IO {
+    public static void println() {
+        System.out.println();
+    }
+
+    public static void println(Object value) {
+        System.out.println(value);
+    }
+
+    public static void print(Object value) {
+        System.out.print(value);
+    }
+}
