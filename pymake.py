a48523bbb287222deb61578e550f479535d83fcb
NASCC <pedro.main.tec@gmail.com>
2025-02-20 18:19:52 -0300

    [UPDATED] Testing command existence

--- pymake.py
+++ pymake.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 
+# required lib to command exists check
+import shutil as st
+
+# required commands for our script
+COMPILE_CMD = "javac"
+RUN_CMD = "java"
+
+def command_exists(command_name: str) -> bool:
+    return st.which(command_name) is not None
+
 def main():
-    print("Hello, World!")
+
+    # if compile command doesn't exists
+    if not command_exists(COMPILE_CMD):
+        print(f"  \x1b[1;32m{COMPILE_CMD}\x1b[0m command not found. \x1b[1;31mAborting\x1b[0m!")
+        quit()
+
+    # if run command doesn't exists
+    if not command_exists(RUN_CMD):
+        print(f"  \x1b[1;32m{RUN_CMD}\x1b[0m command not found. \x1b[1;31mAborting\x1b[0m!")
+        quit()
+
+    print("Everything will work \x1b[1;32mout fine\x1b[0m!")
 
 if __name__ == "__main__":
     main()
