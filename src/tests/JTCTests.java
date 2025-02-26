7b6f799d5e33fc60729c1ec029618c8b9df1e875
NASCC <pedro.main.tec@gmail.com>
2025-02-26 20:54:39 -0300

    [UPDATED] Using external packages import

--- src/tests/JTCTests.java
+++ src/tests/JTCTests.java
@@ -1,7 +1,11 @@
+import nasccped.jtc.utils.IO;
+
 public class JTCTests {
+
+    private static final IO io = new IO();
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the \u001b[1;31mJTC testing\u001b[0m section!");
+        io.println("Welcome to the \u001b[1;31mJTC testing\u001b[0m section!");
     }
 }
