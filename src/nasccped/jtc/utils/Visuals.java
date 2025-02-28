a25f8993a79930e965310bf468f214970d7ce894
NASCC <pedro.main.tec@gmail.com>
2025-02-28 11:11:23 -0300

    [UPDATED] Reusable const values (color escapes) added

--- src/nasccped/jtc/utils/Visuals.java
+++ src/nasccped/jtc/utils/Visuals.java
@@ -1,7 +1,14 @@
 package nasccped.jtc.utils;
 
 public class Visuals {
+
+    private final String clearEscape = "\u001b[2J\u001b[H";
+
+    public final String resetEscape = "\u001b[0m";
+    public final String boldMagenta = "\u001b[1;35m";
+    public final String boldWhite = "\u001b[1;37m";
+
     public void clearTerminal() {
-        System.out.print("\u001b[2J\u001b[H");
+        System.out.print(clearEscape);
     }
 }
