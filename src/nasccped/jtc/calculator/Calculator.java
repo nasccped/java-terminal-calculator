81902622f25f053e3bf8949377bca422f21275df
NASCC <pedro.main.tec@gmail.com>
2025-02-28 10:50:50 -0300

    [UPDATED] Using the new implemented util (String manip.)

--- src/nasccped/jtc/calculator/Calculator.java
+++ src/nasccped/jtc/calculator/Calculator.java
@@ -1,20 +1,32 @@
 package nasccped.jtc.calculator;
 
 import nasccped.jtc.utils.IO;
+import nasccped.jtc.utils.StringUtils;
 
 public class Calculator {
 
     private final IO io = new IO(2);
+    private final StringUtils stut = new StringUtils();
 
     public void showUI() {
 
         io.println();
-        io.println("\u001b[1;35mCalculator\u001b[0m");
-        io.println("\u001b[1;35m==========\u001b[0m");
+        io.println(stut.concat("\u001b[1;35m",
+                               "Calculator",
+                               "\u001b[0m"));
+
+        
+        io.println(stut.concat("\u001b[1;35m",
+                               "==========",
+                               "\u001b[0m"));
         io.println();
 
         io.println("This is the terminal calculator!");
-        io.println("I'm still \u001b[1;37mworking on it!\u001b[0m");
+        io.println(stut.concat("I'm still ",
+                               "\u001b[1;37m",
+                               "working on it!",
+                               "\u001b[0m"));
+
         io.println();
     }
 }
