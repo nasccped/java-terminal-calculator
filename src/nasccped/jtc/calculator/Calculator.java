5b8a0b0260d788d12f50462b41b0f3090c513509
NASCC <pedro.main.tec@gmail.com>
2025-02-28 11:11:56 -0300

    [UPDATED] Using Visuals class const values

--- src/nasccped/jtc/calculator/Calculator.java
+++ src/nasccped/jtc/calculator/Calculator.java
@@ -2,30 +2,32 @@ package nasccped.jtc.calculator;
 
 import nasccped.jtc.utils.IO;
 import nasccped.jtc.utils.StringUtils;
+import nasccped.jtc.utils.Visuals;
 
 public class Calculator {
 
     private final IO io = new IO(2);
+    private final Visuals vsl = new Visuals();
     private final StringUtils stut = new StringUtils();
 
     public void showUI() {
 
         io.println();
-        io.println(stut.concat("\u001b[1;35m",
+        io.println(stut.concat(vsl.boldMagenta,
                                "Calculator",
-                               "\u001b[0m"));
+                               vsl.resetEscape));
 
         
-        io.println(stut.concat("\u001b[1;35m",
+        io.println(stut.concat(vsl.boldMagenta,
                                "==========",
-                               "\u001b[0m"));
+                               vsl.resetEscape));
         io.println();
 
         io.println("This is the terminal calculator!");
         io.println(stut.concat("I'm still ",
-                               "\u001b[1;37m",
+                               vsl.boldWhite,
                                "working on it!",
-                               "\u001b[0m"));
+                               vsl.resetEscape));
 
         io.println();
     }
