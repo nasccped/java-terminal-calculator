a054546d5e9ae1d4b90da8fd99aa17bf929e91f7
NASCC <pedro.main.tec@gmail.com>
2025-02-28 10:22:29 -0300

    [UPDATED] Importing new feature (calculator class) / removing useless one

--- src/nasccped/jtc/JavaTermCalc.java
+++ src/nasccped/jtc/JavaTermCalc.java
@@ -1,18 +1,14 @@
 package nasccped.jtc;
 
-import nasccped.jtc.utils.Visuals;
-import nasccped.jtc.utils.IO;
+import nasccped.jtc.calculator.Calculator;
 
 public class JavaTermCalc {
 
-    private static final IO io = new IO();
-    private static final Visuals vsl = new Visuals();
+    private static final Calculator calc = new Calculator();
 
     public static void main(String[] args) {
+
         vsl.clearTerminal();
-        io.println();
-        io.println("  Hello, calculator,");
-        io.println("  From a \u001b[1;33mnew branch(dev)\u001b[0m!");
-        io.println();
+        calc.showUI();
     }
 }
