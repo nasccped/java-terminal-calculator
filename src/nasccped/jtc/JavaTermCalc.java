f57233304da09d9e20b7cbc126c0661038e4c9c8
NASCC <pedro.main.tec@gmail.com>
2025-02-28 20:48:51 -0300

    [UPDATED] Using the calculator main run function

--- src/nasccped/jtc/JavaTermCalc.java
+++ src/nasccped/jtc/JavaTermCalc.java
@@ -1,16 +1,12 @@
 package nasccped.jtc;
-
-import nasccped.jtc.calculator.Calculator;
+import nasccped.jtc.calculator.MainCalculator;
 import nasccped.jtc.utils.Visuals;
 
 public class JavaTermCalc {
 
-    private static final Calculator calc = new Calculator();
-    private static final Visuals vsl = new Visuals();
+    private static final MainCalculator mcalc = new MainCalculator();
 
     public static void main(String[] args) {
-
-        vsl.clearTerminal();
-        calc.showUI();
+        mcalc.init();
     }
 }
