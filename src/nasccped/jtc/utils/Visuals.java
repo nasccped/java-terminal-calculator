d7f0e2702680c9dfcd4e5f7d69d53bd855bdc1ea
NASCC <pedro.main.tec@gmail.com>
2025-02-28 21:06:27 -0300

    [UPDATED] New feat. (get escape by color name)

--- src/nasccped/jtc/utils/Visuals.java
+++ src/nasccped/jtc/utils/Visuals.java
@@ -1,12 +1,27 @@
 package nasccped.jtc.utils;
+import java.util.HashMap;
 
 public class Visuals {
 
     private final String clearEscape = "\u001b[2J\u001b[H";
+    private final HashMap<String, String> bodyColorEscapes = new HashMap<>();
 
-    public final String resetEscape = "\u001b[0m";
-    public final String boldMagenta = "\u001b[1;35m";
-    public final String boldWhite = "\u001b[1;37m";
+    // fill the hashMap on object creation
+    public Visuals() {
+        bodyColorEscapes.put("reset"  , "\u001b[0m"   );
+        bodyColorEscapes.put("red"    , "\u001b[1;31m");
+        bodyColorEscapes.put("green"  , "\u001b[1;32m");
+        bodyColorEscapes.put("yellow" , "\u001b[1;33m");
+        bodyColorEscapes.put("blue"   , "\u001b[1;34m");
+        bodyColorEscapes.put("magenta", "\u001b[1;35m");
+        bodyColorEscapes.put("cyan"   , "\u001b[1;36m");
+        bodyColorEscapes.put("white"  , "\u001b[1;37m");
+    }
+
+    public String getBodyColorByName(String name) {
+        String result = bodyColorEscapes.get(name.strip().toLowerCase());
+        return result == null ? "\u001b[1;33;41m" : result;
+    }
 
     public void clearTerminal() {
         System.out.print(clearEscape);
