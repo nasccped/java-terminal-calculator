00f82121a96f8e564447265b6696837324466be9
Nasccped <pedro.main.tec@gmail.com>
2025-06-11 22:26:20 -0300

    feat(IO-Builder dp): use a Builder design pattern to handle IO obj creation

--- src/JavaTermCalc.java
+++ src/JavaTermCalc.java
@@ -1,3 +1,5 @@
+import io.IO;
+
 /**
  * The program entrypoint
  *
@@ -7,21 +9,35 @@
 public class JavaTermCalc {
 
     /**
-     * Main IO obj from
+     * Main IO obj
      *
      * <p>Should be static and will be used on the entire program (this
      * instance only).</p>
      */
-    private static final IO io = new IO(2);
+    private static IO io;
 
     public static void main(String[] args) {
-        String projectName = "JavaTermCalc";
-        io.println("Welcome to " + projectName);
+        try {
+            io = IO.builder()        // new keyword isn't necessary
+                .defaultPrompt("> ") // the object is created within a
+                .identPadding(2)     // static method
+                .build();
+        } catch (Exception e) {
+            System.out.println("Error trying to create the IO object:");
+            System.out.println("> \u001b[1;91m" + e.getMessage() + "\u001b[0m");
+            return;
+        }
+        String welcomeTitle = String.format("Welcome to the %s%s%s!",
+                                            "\u001b[1;38;5;153m",
+                                            "Java-Terminal-Calculator",
+                                            "\u001b[0m");
+        io.println(welcomeTitle);
+        String name = io.promptedInput("Say your name: ");
+        if (name.strip().equalsIgnoreCase("Heisenberg")) {
+            io.println("\u001b[1;32mWe need to cook\u001b[0m -_-");
+        } else {
+            io.println("Hi, " + name + "!");
+        }
         io.println();
-        io.setNextPadding(4);
-        io.print("This is a ");
-        io.print("printing with four ");
-        io.println("tab spaces!");
-        io.println("And now, it's normal");
     }
 }
---
+++ src/io/NonSetIdentPaddingException.java
@@ -0,0 +1,7 @@
+package io;
+
+class NonSetIdentPaddingException extends Exception {
+    public NonSetIdentPaddingException(String errorMessage) {
+        super(errorMessage);
+    }
+}
