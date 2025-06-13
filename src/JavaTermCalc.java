f58dc66d3ad2b538bd7a3da78326e84d9ff87593
Nasccped <pedro.main.tec@gmail.com>
2025-06-13 20:05:04 -0300

    chore: call Calculator entrypoint

--- src/JavaTermCalc.java
+++ src/JavaTermCalc.java
@@ -1,4 +1,5 @@
 import io.IO;
+import calculator.Calculator;
 
 /**
  * The program entrypoint
@@ -27,17 +28,6 @@ public class JavaTermCalc {
             System.out.println("> \u001b[1;91m" + e.getMessage() + "\u001b[0m");
             return;
         }
-        String welcomeTitle = String.format("Welcome to the %s%s%s!",
-                                            "\u001b[1;38;5;153m",
-                                            "Java-Terminal-Calculator",
-                                            "\u001b[0m");
-        io.println(welcomeTitle);
-        String name = io.promptedInput("Say your name: ");
-        if (name.strip().equalsIgnoreCase("Heisenberg")) {
-            io.println("\u001b[1;32mWe need to cook\u001b[0m -_-");
-        } else {
-            io.println("Hi, " + name + "!");
-        }
-        io.println();
+        Calculator.run(io);
     }
 }
