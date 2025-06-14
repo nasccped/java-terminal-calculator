8663d37f902ea1594cf955d3db87206cec228aa2
Nasccped <pedro.main.tec@gmail.com>
2025-06-13 21:04:54 -0300

    chore: test IO polished String generation

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -18,12 +18,30 @@ public class Calculator {
     // inner io object
     private static IO io;
 
+    // store polished userInput
+    private static String userInput;
+
     /**
      * Calculator class 'main' function
      */
     public static void run(IO ioObject) {
-        // set and run for testing
         io = ioObject;
-        io.println("Log from Calculator class!");
+
+        // polished String loop testing
+        do {
+            io.clearTerm();
+
+            io.print("Previous input: ");
+            io.println(String.format("\u001b[1;97;44m%s\u001b[0m\n",
+                                     userInput == null ? "None" : userInput));
+
+            userInput = io.promptedInput("Say something - "
+                                         + "'\u001b[3;92m:quit\u001b[0m' "
+                                         + "to exit: ");
+        } while (!userInput.equalsIgnoreCase(":quit"));
+
+        // bye log
+        io.clearTerm();
+        io.println("\nbye\n");
     }
 }
