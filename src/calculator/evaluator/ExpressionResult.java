ab3a362b8cb1661ea2e174ae3f0c866b8a89f3f7
Nasccped <pedro.main.tec@gmail.com>
2025-07-05 11:49:51 -0300

    chore: add expression utilities (read commit message)
    The expression itself will need more features, such as:
    - is it an ok expr?
    - what's the error kind?
    - others.
    
    All this stuff will be used by the UI class for pretty printing!

---
+++ src/calculator/evaluator/ExpressionResult.java
@@ -0,0 +1,74 @@
+package calculator.evaluator;
+
+/**
+ * Store all the expression output data (expression itself, the result, the
+ * status and 'error-at')
+ *
+ * <p>
+ * This class is used by the UI class at `calculator.ui` package. It's usefull
+ * when printing not only the final result, but also the expression info.
+ * </p>
+ */
+public class ExpressionResult {
+
+    // expression input (post-normalize)
+    private String expression;
+    // result of the evaluated expression
+    private double result;
+    // status of the given expression ( OK | ERR_<ERROR_TYPE> )
+    private ExpressionStatus status;
+
+    // error occurs between (start..end) (both -1 if no error)
+    private int errorStart;
+    private int errorEnd;
+
+    /**
+     * Generate a ExpressionResult with its default values
+     *
+     * <p>
+     * This function is used only at Calculator.run() ('calculator' package).
+     *
+     * It generates default values when the program starts. The inner values
+     * can only be changed using the setter function 'setNewValues'.
+     * </p>
+     */
+    public ExpressionResult() {
+        this.expression = "";
+        this.result = 0.0;
+        this.status = ExpressionStatus.WELCOME;
+        this.errorStart = -1;
+        this.errorEnd = -1;
+    }
+
+    public String getExpression() { return expression; }
+
+    public int errorStartAt() { return errorStart; }
+
+    public int errorEndAt() { return errorEnd; }
+
+    public ExpressionStatus getStatus() { return status; }
+
+    public double getResult() { return result; }
+
+    /**
+     * Set new values to this object
+     *
+     * <p>
+     * This function is only used by the 'Evaluator' class of this same
+     * package. It explain the reason of the protected visibility. Outside
+     * classes can't change this class by his own.
+     * </p>
+     */
+    protected void setNewValues(String newExp,
+                                double newRes,
+                                ExpressionStatus newStts,
+                                int newErrorStrt,
+                                int newErrorEnd) {
+        expression = newExp;
+        result = newRes;
+        status = newStts;
+        errorStart = newErrorStrt;
+        errorEnd = newErrorEnd;
+    }
+
+}
---
+++ src/calculator/evaluator/ExpressionStatus.java
@@ -0,0 +1,45 @@
+package calculator.evaluator;
+
+/**
+ * Stores the status of the given expression:
+ *
+ * <p>
+ * When everything goes right, this enum is useless, otherwise, the user need
+ * to know what they did wrong.
+ *
+ * This enum will store possible error causes, such as:
+ * - division by 0 ("2 / (1 - 1)")
+ * - invalid input ("2 + NaN")
+ * - any other error prone...
+ *
+ * It also have method utilities:
+ * - isOk: expression is value
+ * - isWelcome: for welcome message loading
+ * - unwrapMessage: get some String message based on the enum itself
+ *
+ * NOTE: this enum and its functions will be used by the UI class, so it must
+ *       be public!
+ *
+ * </p>
+ */
+public enum ExpressionStatus {
+    OK,
+    WELCOME,
+    ERR_EMPTY_EXPRESSION,
+    TEST_CASE;
+
+    public boolean isOk() { return this == OK; }
+    public boolean isWelcome() { return this == WELCOME; }
+
+    public String unwrapMessage() {
+        switch (this) {
+            case OK:
+                return "Is everythin OK with this expression";
+            case WELCOME:
+                return "Welcome! You can use ':quit' to exit this program";
+            case EMPTY_EXPRESSION:
+                return "This is a test...";
+        }
+        return "FALLBACK STRING";
+    }
+}
