71f085fdb56d2c81032fc75c6d25c562786c0c14
Nasccped <pedro.main.tec@gmail.com>
2025-07-13 11:50:09 -0300

    chore: starts checker package

---
+++ src/calculator/evaluator/checker/Checker.java
@@ -0,0 +1,19 @@
+package calculator.evaluator.parser;
+
+import calculator.evaluator.tokenizer.Token;
+import calculator.evaluator.tokenizer.TokenKind;
+
+/**
+ * Preparatory checks for normalized input (paren count, token precedence, ...)
+ */
+public class PreparatoryChecker {
+
+    private static TokenKind errorKind;
+    private static int errorStart;
+    private static int errorEnd;
+
+    public static void checkTokens(List<Token> input) {
+        // TODO: implement code here...
+    }
+
+}
