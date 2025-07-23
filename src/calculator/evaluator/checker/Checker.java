f7b5646e94dad0ec0b4e00172cb604c54ecf38fc
Nasccped <pedro.main.tec@gmail.com>
2025-07-22 21:56:31 -0300

    chore(func. feats.): check invalid tokens + check parenthesis group

--- src/calculator/evaluator/checker/Checker.java
+++ src/calculator/evaluator/checker/Checker.java
@@ -3,17 +3,61 @@ package calculator.evaluator.checker;
 import calculator.evaluator.tokenizer.Token;
 import calculator.evaluator.tokenizer.TokenKind;
 
+import java.util.stream.Collectors;
+import java.util.List;
+
+
 /**
  * Preparatory checks for normalized input (paren count, token precedence, ...)
  */
 public class Checker {
 
-    private static TokenKind errorKind;
-    private static int errorStart;
-    private static int errorEnd;
-
-    public static void checkTokens(List<Token> input) {
-        // TODO: implement code here...
+    /**
+     * Get indexes of the firs ivalid token in a given list, else return empty
+     * array
+     */
+    public static int[] invalidToken(List<Token> input) {
+        return input
+            .stream()
+            .filter(Token::isInvalid)
+            .findFirst()
+            .map(Token::getIndexRange)
+            .orElse(new int[0]);
     }
 
+    /**
+     * Get indexes of invalid parenthesis group
+     */
+    public static int[] invalidParenthesis(List<Token> input) {
+        List<Token> parens =input
+                .stream()
+                .filter(v ->
+                    v.getKind() == TokenKind.OPN_PAREN
+                    || v.getKind() == TokenKind.CLS_PAREN)
+                .collect(Collectors.toList());
+        if (parens.isEmpty()) return new int[0];
+        int parenCount = 0;
+        int[] range = {
+            0, parens.get(parens.size() - 1).getEndInd()
+        };
+        boolean isIncreasing;
+        for (Token tk : parens) {
+            if (tk.getKind() == TokenKind.OPN_PAREN) {
+                parenCount++;
+                isIncreasing = true;
+            }
+            else {
+                parenCount--;
+                isIncreasing = false;
+            }
+
+            if (parenCount < 0) {
+                range[0] = tk.getStartInd();
+                range[1] = tk.getEndInd();
+                break;
+            }
+            if (parenCount == 1 && isIncreasing) range[0] = tk.getStartInd();
+        }
+        return parenCount == 0 ? new int[0] : range;
+    }
 }
