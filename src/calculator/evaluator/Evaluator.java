f8f77f004821b78c58582e382d369e208f343e0a
Nasccped <pedro.main.tec@gmail.com>
2025-07-22 22:04:10 -0300

    feat: set predictable errors in expression output

--- src/calculator/evaluator/Evaluator.java
+++ src/calculator/evaluator/Evaluator.java
@@ -1,6 +1,9 @@
 package calculator.evaluator;
 
 import calculator.evaluator.inputhandler.InputNormalizer;
+import calculator.evaluator.checker.Checker;
+import calculator.evaluator.tokenizer.Token;
+import java.util.List;
 
 /**
  * The expression evaluator entrypoint.
@@ -23,12 +26,34 @@ public class Evaluator {
          *       I need to evaluate and get the expression results to set in
          *       the ExpressionStatus object.
          */
+        String normalized = InputNormalizer.normalize(express);
+        setOn.setStatus(ExpressionStatus.OK);
+        setOn.setExpression(normalized);
+        setOn.setResult(0.0);
+        setOn.setErrorStart(-1);
+        setOn.setErrorEnd(-1);
 
-        // set
-        setOn.setExpression(InputNormalizer.normalize(express)); // expression
-        setOn.setResult(0.0); // result
-        setOn.setStatus(ExpressionStatus.TEST_CASE); // status
-        setOn.setErrorStart(-1); // err start
-        setOn.setErrorEnd(-1); // err end
+        List<Token> tkList = InputNormalizer.strToTokenList(normalized);
+        int[] errInds;
+        // test tokens
+        errInds = Checker.invalidToken(tkList);
+        if (errInds.length > 0) {
+            setErrorIndexes(setOn, errInds);
+            setOn.setStatus(ExpressionStatus.ERR_INVALID_TOKEN);
+            return;
+        }
+        // test parenthesis
+        errInds = Checker.invalidParenthesis(tkList);
+        if (errInds.length > 0) {
+            setErrorIndexes(setOn, errInds);
+            setOn.setStatus(ExpressionStatus.ERR_MISSING_PAREN);
+            return;
+        }
+        // others tests needed
+    }
+
+    private static void setErrorIndexes(ExpressionResult setOn, int[] values) {
+            setOn.setErrorStart(values[0]);
+            setOn.setErrorEnd(values[1]);
     }
 }
