ec52a41f375361abf7720327e4b1dfc93750b5ae
Nasccped <pedro.main.tec@gmail.com>
2025-07-27 11:37:24 -0300

    chore: implement entire eval steps

--- src/calculator/evaluator/Evaluator.java
+++ src/calculator/evaluator/Evaluator.java
@@ -1,10 +1,12 @@
 package calculator.evaluator;
 
-import calculator.evaluator.inputhandler.InputNormalizer;
 import calculator.evaluator.checker.Checker;
-import calculator.evaluator.tokenizer.Token;
+import calculator.evaluator.evaluable.DivisionByZero;
 import calculator.evaluator.expression.ExpressionResult;
 import calculator.evaluator.expression.ExpressionStatus;
+import calculator.evaluator.inputhandler.InputNormalizer;
+import calculator.evaluator.tokenizer.Token;
+import calculator.evaluator.tree.Tree;
 import java.util.List;
 
 /**
@@ -18,6 +20,8 @@ import java.util.List;
 public class Evaluator {
 
     private static final int[] defaultErrorRange = {-1, -1};
+    private static double result;
+    private static Tree expressionTree;
 
     /**
      * Take the user input, evaluate and apply the final result into an extern
@@ -25,39 +29,47 @@ public class Evaluator {
      */
     public static void setExpressionResult(String express,
                                            ExpressionResult setOn) {
-        /**
-         * TODO: this is temporary (set sample values).
-         *       I need to evaluate and get the expression results to set in
-         *       the ExpressionStatus object.
-         */
+        // set values first (prevent not changed attributes if expresion fails)
         String normalized = InputNormalizer.normalize(express);
         setOn.setStatus(ExpressionStatus.OK);
         setOn.setExpression(normalized);
         setOn.setResult(0.0);
         setOn.setErrorRange(defaultErrorRange);
 
-        List<Token> tkList = InputNormalizer.strToTokenList(normalized);
+        List<Token> expressionTokenList = InputNormalizer
+            .strToTokenList(normalized);
+        // if secure expression check fails, stop function
+        if (handleChecks(expressionTokenList, setOn)) return;
 
-        if (tkList.isEmpty()) {
+        expressionTree = new Tree(expressionTokenList);
+
+        try {
+            result = expressionTree.evaluate();
+            setOn.setResult(result);
+        } catch (DivisionByZero e) {
+            setOn.setErrorRange(e.getIndexRange());
+            setOn.setStatus(ExpressionStatus.ERR_DIVISION_BY_ZERO);
+        }
+    }
+
+    /**
+     * Do all the check stuff in a separated function + set status for each of
+     * them.
+     */
+    private static boolean handleChecks(List<Token> exprList,
+                                        ExpressionResult setOn) {
+        if (exprList.isEmpty())
             setOn.setStatus(ExpressionStatus.ERR_EMPTY_EXPRESSION);
-            return;
-        }
-        if (Checker.invalidToken(tkList, setOn)) {
+        else if (Checker.invalidToken(exprList, setOn))
             setOn.setStatus(ExpressionStatus.ERR_INVALID_TOKEN);
-            return;
-        }
-        if (Checker.missingParenthesis(tkList, setOn)) {
+        else if (Checker.missingParenthesis(exprList, setOn))
             setOn.setStatus(ExpressionStatus.ERR_MISSING_PAREN);
-            return;
-        }
-        if (Checker.tokenProbablyMissing(tkList, setOn)) {
+        else if (Checker.tokenProbablyMissing(exprList, setOn))
             setOn.setStatus(ExpressionStatus.ERR_MISSING_TOKEN);
-            return;
-        }
-        if (Checker.invalidOperPos(tkList, setOn)) {
+        else if (Checker.invalidOperPos(exprList, setOn))
             setOn.setStatus(ExpressionStatus.ERR_OPER_AT_SIDES);
-            return;
-        }
-        // others tests needed
+        else return false;
+
+        return true;
     }
 }
