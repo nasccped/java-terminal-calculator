e83079fae3a3533cbdfaf793451ea48eedad2208
Nasccped <pedro.main.tec@gmail.com>
2025-07-27 11:35:00 -0300

    chore/fix: fix index extraction + use better indenting

--- src/calculator/evaluator/checker/Checker.java
+++ src/calculator/evaluator/checker/Checker.java
@@ -1,13 +1,11 @@
 package calculator.evaluator.checker;
 
+import calculator.evaluator.expression.ExpressionResult;
 import calculator.evaluator.tokenizer.Token;
 import calculator.evaluator.tokenizer.TokenKind;
-import calculator.evaluator.expression.ExpressionResult;
-
-import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
 
 /**
  * Preparatory checks for normalized input (paren count, token precedence, ...)
@@ -15,10 +13,11 @@ import java.util.Optional;
 public class Checker {
 
     /**
-     * Get indexes of the firs ivalid token in a given list, else return empty
-     * array
+     * Get indexes of the firs ivalid token in the given list, else return
+     * empty array
      */
-    public static boolean invalidToken(List<Token> input, ExpressionResult setOn) {
+    public static boolean invalidToken(List<Token> input,
+                                       ExpressionResult setOn) {
         Optional<Token> invalid = input
             .stream()
             .filter(Token::isInvalid)
@@ -31,21 +30,21 @@ public class Checker {
     }
 
     /**
-     * Get indexes of invalid parenthesis group
+     * Get indexes of invalid parenthesis (missing opn or cls paren)
      */
-    public static boolean missingParenthesis(List<Token> input, ExpressionResult setOn) {
+    public static boolean missingParenthesis(List<Token> input,
+                                             ExpressionResult setOn) {
         List<Token> parens = input
             .stream()
-            .filter(v ->
-                v.getKind() == TokenKind.OPN_PAREN
-                || v.getKind() == TokenKind.CLS_PAREN
-            ).collect(Collectors.toList());
+            .filter(v -> v.getKind() == TokenKind.OPN_PAREN
+                      || v.getKind() == TokenKind.CLS_PAREN)
+            .collect(Collectors.toList());
 
         if (parens.isEmpty()) return false;
 
         int parenCount = 0;
         int[] range = {
-            0, parens.get(parens.size() - 1).getEndInd()
+            0, parens.get(parens.size() - 1).getIndexRange()[1]
         };
         boolean isIncreasing;
         for (Token tk : parens) {
@@ -56,13 +55,12 @@ public class Checker {
                 parenCount--;
                 isIncreasing = false;
             }
-
             if (parenCount < 0) {
-                range[0] = tk.getStartInd();
-                range[1] = tk.getEndInd();
+                range[1] = tk.getIndexRange()[1];
                 break;
             }
-            if (parenCount == 1 && isIncreasing) range[0] = tk.getStartInd();
+            if (parenCount == 1 && isIncreasing)
+                range[0] = tk.getIndexRange()[0];
         }
         if (parenCount != 0) {
             setOn.setErrorRange(range);
@@ -71,53 +69,50 @@ public class Checker {
         return false;
     }
 
-    public static boolean invalidOperPos(List<Token> input, ExpressionResult setOn) {
-        if (input.isEmpty()) return false;
-
-        int[] range = new int[2];
+    /**
+     * Check operators at invalid positions (starts or ends with '-', '*', ...)
+     */
+    public static boolean invalidOperPos(List<Token> input,
+                                         ExpressionResult setOn) {
+        // NOTE: this is perfectly fine since input will never be empty
+        // check for latest token
         Token tk = input.get(input.size() - 1);
-
         switch (tk.getKind()) {
             case ADD_SIGN:
             case SUB_SIGN:
             case DIV_SIGN:
             case MUL_SIGN:
             case POW_SIGN:
-                range[0] = tk.getStartInd();
-                range[1] = tk.getEndInd();
-                setOn.setErrorRange(range);
+                setOn.setErrorRange(tk.getIndexRange());
                 return true;
         }
-
+        // check for first token
         tk = input.get(0);
-
         switch (tk.getKind()) {
             case POW_SIGN:
             case DIV_SIGN:
             case MUL_SIGN:
-                range[0] = tk.getStartInd();
-                range[1] = tk.getEndInd();
-                setOn.setErrorRange(range);
+                setOn.setErrorRange(tk.getIndexRange());
                 return true;
             case ADD_SIGN:
             case SUB_SIGN:
                 if (input.size() == 1) {
-                    range[0] = tk.getStartInd();
-                    range[1] = tk.getEndInd();
-                    setOn.setErrorRange(range);
+                    setOn.setErrorRange(tk.getIndexRange());
                     return true;
                 }
                 if (input.get(1).getKind() != TokenKind.OPN_PAREN) {
-                    range[0] = tk.getStartInd();
-                    range[1] = tk.getEndInd();
-                    setOn.setErrorRange(range);
+                    setOn.setErrorRange(tk.getIndexRange());
                     return true;
                 }
         }
         return false;
     }
 
-    public static boolean tokenProbablyMissing(List<Token> input, ExpressionResult setOn) {
+    /**
+     * Check tokens probably missing (ie, "32 2.2" is missing an operator)
+     */
+    public static boolean tokenProbablyMissing(List<Token> input,
+                                               ExpressionResult setOn) {
         if (input.size() < 2) return false;
         Token curr, prev;
         int[] range = new int[2];
@@ -133,7 +128,8 @@ public class Checker {
             else if (
                 prev.isOper()
                 && curr.getKind() == TokenKind.CLS_PAREN) isErr = true;
-            // open parenthesis followed by close parenthesis (empty parenthesis)
+            // open parenthesis followed by close parenthesis (empty
+            // parenthesis)
             else if (
                 prev.getKind() == TokenKind.OPN_PAREN
                 && curr.getKind() == TokenKind.CLS_PAREN) isErr = true;
@@ -143,6 +139,9 @@ public class Checker {
             // operator followed by an operator
             else if (
                 curr.isOper() && prev.isOper()) isErr = true;
+            else if (
+                prev.getKind() == TokenKind.OPN_PAREN
+                && curr.isOper()) isErr = true;
 
             // if any of the conditions above
             if (isErr) {
